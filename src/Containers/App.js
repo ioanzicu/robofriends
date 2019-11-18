4aaf8b5db654e16fec391d499ae62e39a39481c3
Ioan <ioanzicu94@gmail.com>
2019-11-18 22:01:02 +0300

    ...

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,33 +1,25 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import MainPage from '../Components/MainPage'
+import React from "react";
+import { connect } from "react-redux";
+import MainPage from "../Components/MainPage";
 
-import { setSearchField, requestRobots } from '../actions'
+import { setSearchField, requestRobots } from "../actions";
 
-const App = props => <MainPage {...props} />
+const App = props => <MainPage {...props} />;
 
 const mapStateToProps = ({ searchRobotsReducer, requestRobotsReducer }) => {
   return {
     searchField: searchRobotsReducer.searchField,
-    
-    robots: 
-    
-    
-    requestRobotsReducer.robots,
+    robots: requestRobotsReducer.robots,
     isPending: requestRobotsReducer.isPending,
-    error:  requestRobotsReducer.error
-  }
-}
+    error: requestRobotsReducer.error
+  };
+};
 
-const mapDispatchToProps =   dispatch => {
+const mapDispatchToProps = dispatch => {
   return {
     onSearchChange: event => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => 
-    dispatch(requestRobots())
-  }
-}
+    onRequestRobots: () => dispatch(requestRobots())
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
